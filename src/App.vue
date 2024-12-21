f5cf2b382793e4c3e4fd334955d8a65fdf19fdcd
amanda-santos <amanda.santosf.dev@gmail.com>
2024-12-20 23:28:25 -0300

    feat: ✨ add logs and responsiveness

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,17 @@ import { ref, computed, watch } from 'vue'
 import CustomButton from './components/CustomButton.vue'
 import { getRandomValue } from './helpers/getRandomValue'
 
+type LogAction = {
+  actor: 'player' | 'monster'
+  action: 'attack' | 'heal'
+  value?: number
+}
+
 const currentRound = ref(0)
 const playerHealth = ref(100)
 const monsterHealth = ref(100)
 const winner = ref<'player' | 'monster' | 'draw' | null>(null)
-const monsterImageAnimation = ref('')
+const logs = ref<LogAction[]>([])
 
 const monsterBarStyles = computed(() => {
   if (monsterHealth.value <= 0) {
@@ -25,32 +31,18 @@ const playerBarStyles = computed(() => {
   return { width: `${playerHealth.value}%` }
 })
 
-// watch(monsterHealth, (newValue, oldValue) => {
-//   if (newValue < oldValue) {
-//     monsterImageAnimation.value = 'animate-pulse'
-//   }
-
-//   if (newValue > oldValue) {
-//     monsterImageAnimation.value = 'animate-bounce'
-//   }
-
-//   setTimeout(() => {
-//     monsterImageAnimation.value = ''
-//   }, 2000)
-// })
-
 const canUseSpecialAttack = computed(() => currentRound.value % 3 !== 0)
+
+const addLog = (logAction: LogAction) => {
+  logs.value.push(logAction)
+}
 
 const attackPlayer = () => {
   const attackValue = getRandomValue(8, 15)
 
   playerHealth.value -= attackValue
 
-  monsterImageAnimation.value = 'animate-ping'
-
-  setTimeout(() => {
-    monsterImageAnimation.value = ''
-  }, 3000)
+  addLog({ actor: 'monster', action: 'attack', value: attackValue })
 }
 
 const attackMonster = () => {
@@ -60,6 +52,8 @@ const attackMonster = () => {
   monsterHealth.value -= attackValue
 
   attackPlayer()
+
+  addLog({ actor: 'player', action: 'attack', value: attackValue })
 }
 
 const specialAttackMonster = () => {
@@ -69,6 +63,8 @@ const specialAttackMonster = () => {
   monsterHealth.value -= attackValue
 
   attackPlayer()
+
+  addLog({ actor: 'player', action: 'attack', value: attackValue })
 }
 
 const healPlayer = () => {
@@ -82,6 +78,8 @@ const healPlayer = () => {
   } else {
     playerHealth.value = newPlayerHealth
   }
+
+  addLog({ actor: 'player', action: 'heal', value: healValue })
 
   attackPlayer()
 }
@@ -102,56 +100,71 @@ watch([playerHealth, monsterHealth], ([playerHealth, monsterHealth]) => {
 </script>
 
 <template>
-  <main
-    class="flex h-screen flex-col items-center justify-center bg-bg bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:20px_20px] p-20"
+  <div
+    class="flex h-screen flex-col items-center justify-center bg-bg bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:20px_20px] md:p-20"
   >
-    <div class="flex min-w-[400px] flex-col gap-8">
-      <h1 class="mb-4 text-center font-mono text-5xl font-bold uppercase text-text">
-        > Monster Smash
-      </h1>
+    <div class="flex max-w-full flex-col items-center justify-center gap-8 p-4 lg:flex-row">
+      <main class="flex w-full flex-col gap-8 md:min-w-[400px]">
+        <h1 class="mb-4 text-center font-mono text-3xl font-bold uppercase text-text md:text-5xl">
+          > Monster Smash
+        </h1>
 
-      <section class="flex w-full">
-        <img src="./assets/monster.svg" alt="" class="w-24" />
-        <div class="w-full">
-          <h2 class="font-mono font-semibold uppercase text-text">> Monster</h2>
-          <div class="m-1 h-10 w-full border-4 border-border bg-border">
-            <div class="h-full w-full bg-main" :style="monsterBarStyles"></div>
+        <section class="flex w-full">
+          <img src="./assets/monster.svg" alt="" class="hidden w-24 md:block" />
+          <div class="w-full">
+            <h2 class="font-mono font-semibold uppercase text-text">> Monster</h2>
+            <div class="m-1 h-10 w-full border-4 border-border bg-border">
+              <div class="h-full w-full bg-main" :style="monsterBarStyles"></div>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <section class="flex w-full">
-        <img src="./assets/player.svg" alt="" class="w-24" />
-        <div class="w-full">
-          <h2 class="font-mono font-semibold uppercase text-text">> You</h2>
-          <div class="m-1 h-10 w-full border-4 border-border bg-border">
-            <div class="h-full w-full bg-main" :style="playerBarStyles"></div>
+        <section class="flex w-full">
+          <img src="./assets/player.svg" alt="" class="hidden w-24 md:block" />
+          <div class="w-full">
+            <h2 class="font-mono font-semibold uppercase text-text">> You</h2>
+            <div class="m-1 h-10 w-full border-4 border-border bg-border">
+              <div class="h-full w-full bg-main" :style="playerBarStyles"></div>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <section
-        v-if="winner"
-        class="border-2 border-border bg-green-400 p-4 text-center font-mono text-3xl font-bold uppercase text-text"
-        :class="{
-          'bg-green-500': winner === 'player',
-          'bg-red-500': winner === 'monster',
-          'bg-blue-400': winner === 'draw',
-        }"
-      >
-        <h2 v-if="winner === 'monster'">💀 You lost!</h2>
-        <h2 v-else-if="winner === 'player'">🏆 You won!</h2>
-        <h2 v-else-if="winner === 'draw'">It's a draw!</h2>
-      </section>
+        <section
+          v-if="winner"
+          class="w-full border-2 border-border bg-green-400 p-4 text-center font-mono text-3xl font-bold uppercase text-text"
+          :class="{
+            'bg-green-500': winner === 'player',
+            'bg-red-500': winner === 'monster',
+            'bg-blue-400': winner === 'draw',
+          }"
+        >
+          <h2 v-if="winner === 'monster'">💀 You lost!</h2>
+          <h2 v-else-if="winner === 'player'">🏆 You won!</h2>
+          <h2 v-else-if="winner === 'draw'">It's a draw!</h2>
+        </section>
 
-      <section class="flex gap-4" v-else>
-        <custom-button :onClick="attackMonster">Attack</custom-button>
-        <custom-button :onClick="specialAttackMonster" :disabled="canUseSpecialAttack">
-          Special Attack
-        </custom-button>
-        <custom-button :onClick="healPlayer">Heal</custom-button>
-        <custom-button :onClick="surrender">Surrender</custom-button>
-      </section>
+        <section class="flex w-full flex-row flex-wrap gap-4 md:flex-row" v-else>
+          <custom-button :onClick="attackMonster">Attack</custom-button>
+          <custom-button :onClick="specialAttackMonster" :disabled="canUseSpecialAttack">
+            Special Attack
+          </custom-button>
+          <custom-button :onClick="healPlayer">Heal</custom-button>
+          <custom-button :onClick="surrender">Surrender</custom-button>
+        </section>
+      </main>
+
+      <aside class="h-[350px] w-full overflow-scroll bg-zinc-950 p-4 text-white md:min-w-96">
+        <h2 class="font-mono font-semibold uppercase">> Log</h2>
+        <ul class="list-inside font-mono">
+          <li v-for="log in logs" :key="log.action + log.value">
+            <span v-if="log.actor === 'player'">😼 You</span>
+            <span v-else>👾 Monster</span>
+            <span v-if="log.action === 'attack'"> attacked with </span>
+            <span v-else> healed gaining </span>
+            <span v-if="log.value">{{ log.value }} points</span>
+          </li>
+        </ul>
+      </aside>
     </div>
-  </main>
+  </div>
 </template>
